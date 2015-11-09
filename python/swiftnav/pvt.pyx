e735465d85c2777126a66d923dc20fc12a326a5b
Bhaskar Mookerji <mookerji@gmail.com>
2015-11-09 00:40:28 +0000

    Updated Cython bindings for libswiftnav.
    (Also known as the "A bird in the hand is worth two in the bush"
    commit).
    
    A massive monolithic commit (sorry!) containing:
    - Updated coverage on bindings
    - Removes c-specific Cython declaration files and other detritus
    - Very mild unit test coverage on ported structs and functions
    
    Some notes:
    - Documentation is still lacking, but perhaps we can just
      point people at the corresponding C documentation or something.
    - Some of the changes here are trying to minimize boiler plate, such
      as struct initialization for CPython wrapper objects. Many of the
      Cython objects are missing boilerplate setters and getters, but
      hopefully that can be handled some more nicer way.
    - Calling conventions are a bit inconsistent in the bindings and the
      underlying C library. See, I & Q quadratures vs complex p in the
      tracking loop stuff: track.c.
    
    /cc @mfine @fnoble @benjamin0 @kovach @JoshuaGross @henryhallam
    @jacobmcnamee @gsmcmullin @akleeman

--- python/swiftnav/pvt.pyx
+++ python/swiftnav/pvt.pyx
@@ -1,4 +1,4 @@
-# Copyright (C) 2012 Swift Navigation Inc.
+# Copyright (C) 2015 Swift Navigation Inc.
 #
 # This source is subject to the license found in the file 'LICENSE' which must
 # be be distributed together with this source. All other rights reserved.
@@ -7,73 +7,55 @@
 # EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED
 # WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
 
-cimport pvt_c
+# cython: embedsignature=True
+
 from libc.stdlib cimport malloc, free
-
-from common cimport *
-from track_c cimport navigation_measurement_t
+from libc.string cimport memset
 from track cimport NavigationMeasurement
+from track cimport navigation_measurement_t
 
-cdef class Solution:
-  def __repr__(self):
-    return "<Solution %f, %f, %f>" % (57.2957795*self.pos_llh[0],
-                                      57.2957795*self.pos_llh[1],
-                                      self.pos_llh[2])
+cdef class GNSSSolution:
 
-  property pos_llh:
-    def __get__(self):
-      return (self.soln.pos_llh[0],
-              self.soln.pos_llh[1],
-              self.soln.pos_llh[2])
+  def __init__(self, **kwargs):
+    memset(&self._thisptr, 0, sizeof(gnss_solution))
+    if kwargs:
+      self._thisptr = kwargs
 
-  property pos_ecef:
-    def __get__(self):
-      return (self.soln.pos_ecef[0],
-              self.soln.pos_ecef[1],
-              self.soln.pos_ecef[2])
+  def __getattr__(self, k):
+    return self._thisptr.get(k)
 
-  property vel_ned:
-    def __get__(self):
-      return (self.soln.vel_ned[0],
-              self.soln.vel_ned[1],
-              self.soln.vel_ned[2])
 
-  property vel_ecef:
-    def __get__(self):
-      return (self.soln.vel_ecef[0],
-              self.soln.vel_ecef[1],
-              self.soln.vel_ecef[2])
+cdef class DOPS:
 
-  property tow:
-    def __get__(self):
-      return self.soln.time.tow
+  def __init__(self, **kwargs):
+    memset(&self._thisptr, 0, sizeof(dops_t))
+    if kwargs:
+      self._thisptr = kwargs
 
-  property clock_offset:
-    def __get__(self):
-      return self.soln.clock_offset
+  def __getattr__(self, k):
+    return self._thisptr.get(k)
 
-  property clock_bias:
-    def __get__(self):
-      return self.soln.clock_bias
+def calc_PVT_(nav_meas, disable_raim=False):
+  """Wraps the function :libswiftnav:`calc_PVT`.
 
-  property dops:
-    def __get__(self):
-      return (self.dops.pdop,
-              self.dops.gdop,
-              self.dops.tdop,
-              self.dops.hdop,
-              self.dops.vdop)
+  Parameters
+  ----------
+  nav_meas : [NavigationMeasurement]
+    Navigation measurements
+  disable_raim : bool
+    Disable RAIM
 
-def calc_PVT(nav_meas):
+  Returns
+  -------
+  (GNSSSolution, DOPS)
+
+  """
   n_used = len(nav_meas)
-  cdef navigation_measurement_t* nav_meas_array = <navigation_measurement_t*>malloc(n_used*sizeof(navigation_measurement_t))
-
+  cdef navigation_measurement_t* nav_meas_ = <navigation_measurement_t *>malloc(n_used*sizeof(navigation_measurement_t))
   for n in range(n_used):
-    nav_meas_array[n] = (<NavigationMeasurement?>nav_meas[n]).meas
-
-  s = Solution()
-  pvt_c.calc_PVT(n_used, nav_meas_array, False, &(s.soln), &(s.dops))
-
-  free(nav_meas_array)
-
-  return s
+    nav_meas_[n] = (<NavigationMeasurement?>nav_meas[n])._thisptr
+  dops_ = DOPS()
+  soln = GNSSSolution()
+  cdef u8 ret = calc_PVT(n_used, nav_meas_, disable_raim, &soln._thisptr, &dops_._thisptr)
+  free(nav_meas_)
+  return (ret, soln, dops_)
