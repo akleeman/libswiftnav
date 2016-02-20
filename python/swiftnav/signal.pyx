swift-nav/libswiftnav-legacy

commit 2c3bdf602b7fcbe04986b3a528bb7b0976f9977b

Remove global and constellation signal indexing

python/swiftnav/signal.pyx
@@ -39,8 +39,14 @@ cdef class GNSSSignal:
   def is_valid(self):
     return sid_valid(self._thisptr)
 
-  def to_index(self, idx_type):
-    return sid_to_index(self._thisptr, idx_type)
+  def to_code_index(self):
+    return sid_to_code_index(self._thisptr)
+
+  def from_code_index(self, code, code_index):
+    self._thisptr = sid_from_code_index(code, code_index)
+
+  def to_constellation(self):
+    return sid_to_constellation(self._thisptr)
 
   def to_string(self):
     cdef u8 n = 255
@@ -48,9 +54,9 @@ cdef class GNSSSignal:
     cdef s8 length = sid_to_string(&s[0], n, self._thisptr)
     return s[:length].decode('UTF-8')
 
-def sid_from_index(i):
+def signal_from_code_index(code, code_index):
   sid = GNSSSignal()
-  sid._thispr = sid_from_index(i)
+  sid._thisptr = sid_from_code_index(code, code_index)
   return sid
 
 cdef mk_signal_array(py_signals, u8 n_c_signals, gnss_signal_t *c_signals):

include/libswiftnav/signal.h
@@ -23,14 +23,18 @@
 #define NUM_CODES_GPS 2
 #define NUM_CODES_SBAS 1
 
-#define NUM_SIGNALS_GPS (NUM_SATS_GPS * NUM_CODES_GPS)
-#define NUM_SIGNALS_SBAS (NUM_SATS_SBAS * NUM_CODES_SBAS)
+#define NUM_SIGNALS_GPS_L1CA NUM_SATS_GPS
+#define NUM_SIGNALS_GPS_L2CM NUM_SATS_GPS
+#define NUM_SIGNALS_SBAS_L1CA NUM_SATS_SBAS
+
+#define NUM_SIGNALS_GPS (NUM_SIGNALS_GPS_L1CA + NUM_SIGNALS_GPS_L2CM)
+#define NUM_SIGNALS_SBAS (NUM_SIGNALS_SBAS_L1CA)
 #define NUM_SIGNALS (NUM_SIGNALS_GPS + NUM_SIGNALS_SBAS)
 
 #define GPS_FIRST_PRN 1
 #define SBAS_FIRST_PRN 120
 
-#define INVALID_SID_INDEX -1
+#define SID_STR_LEN_MAX 16
 
 enum constellation {
   CONSTELLATION_INVALID = -1,
@@ -46,21 +50,6 @@ enum code {
   CODE_SBAS_L1CA,
   CODE_COUNT,
 };
-
-enum indexing_type {
-  INDEX_GLOBAL, /* [0, NUM_SIGNALS) index of the signal inside the full virtual
-                 * list of all satellites  and codes */
-  INDEX_CONSTELLATION , /* [0, NUM_SIGNALS_CONSTELLATION) index of the signal
-                         * inside the constellation*/
-  INDEX_SAT_IN_CONS, /* [0, NUM_SATS_CONSTELLATION) index of the satellite
-                      * transmitting this sid inside the constellation it
-                      * belongs to*/
-};
-
-#define GPS_FIRST_PRN 1
-#define SBAS_FIRST_PRN 120
-
-#define SID_STR_LEN_MAX 16
 
 typedef struct {
   u16 sat;
@@ -94,21 +83,26 @@ gnss_signal_t construct_sid(enum code code, u16 sat);
  */
 int sid_to_string(char *s, int n, gnss_signal_t sid);
 
-/** Returns true if sid corresponds to a known constellation, band, and
+/** Returns true if sid corresponds to a known constellation, code, and
  *  satellite identifier.
  */
 bool sid_valid(gnss_signal_t sid);
 
-/** Converts the global index i in [0, NUM_SIGNALS) to the corresponding sid
+/** Converts a code-specific index in [0, NUM_SIGNALS_<code>) to
+ *  the corresponding sid.
  */
-gnss_signal_t sid_from_index(u16 i);
+gnss_signal_t sid_from_code_index(enum code code, u16 code_index);
 
-/** Converts sid to a index according to given indexing type.
+/** Returns the code index in [0, NUM_SIGNALS_<code>) corresponding to the sid.
  */
-u16 sid_to_index(gnss_signal_t sid, enum indexing_type it);
+u16 sid_to_code_index(gnss_signal_t sid);
 
-/** Returns the constellation in which the sid belongs to.
+/** Returns the constellation which the sid belongs to.
  */
 enum constellation sid_to_constellation(gnss_signal_t sid);
+
+/** Returns the constellation which the code belongs to.
+ */
+enum constellation code_to_constellation(enum code code);
 
 #endif /* LIBSWIFTNAV_SIGNAL_H */

python/swiftnav/signal.pxd
@@ -11,32 +11,51 @@ from common cimport *
 from libcpp cimport bool
 
 cdef extern from "libswiftnav/signal.h":
-  enum:
+  cdef enum constellation:
+    CONSTELLATION_INVALID = -1
     CONSTELLATION_GPS
     CONSTELLATION_SBAS
     CONSTELLATION_COUNT
+
+  cdef enum code:
+    CODE_INVALID = -1
     CODE_GPS_L1CA
-    CODE_GPS_L2CL
     CODE_GPS_L2CM
     CODE_SBAS_L1CA
     CODE_COUNT
-    INDEX_ALL_CODES
-    INDEX_SATS_IN_CONS
+
+  enum:
+    NUM_SATS_GPS
+    NUM_SATS_SBAS
+    NUM_SATS
+    NUM_CODES_GPS
+    NUM_CODES_SBAS
+    NUM_SIGNALS_GPS_L1CA
+    NUM_SIGNALS_GPS_L2CM
+    NUM_SIGNALS_SBAS_L1CA
+    NUM_SIGNALS_GPS
+    NUM_SIGNALS_SBAS
+    NUM_SIGNALS
+    GPS_FIRST_PRN
+    SBAS_FIRST_PRN
+    SID_STR_LEN_MAX
 
   ctypedef struct gnss_signal_t:
     u16 sat
-    u8 code
+    code code
 
   int sid_compare(const gnss_signal_t a, const gnss_signal_t b)
   int cmp_sid_sid(const void *a, const void *b)
   bool sid_is_equal(const gnss_signal_t a, const gnss_signal_t b)
 
+  gnss_signal_t construct_sid(code code, u16 sat);
   int sid_to_string(char *s, int n, gnss_signal_t sid)
   bool sid_valid(gnss_signal_t sid)
 
-  gnss_signal_t construct_sid(u8 code, u16 sat);
-  gnss_signal_t sid_from_index(u16 i)
-  u16 sid_to_index(gnss_signal_t sid, u8 it)
+  gnss_signal_t sid_from_code_index(code code, u16 code_index);
+  u16 sid_to_code_index(gnss_signal_t sid);
+  constellation sid_to_constellation(gnss_signal_t sid);
+  constellation code_to_constellation(code code);
 
 cdef class GNSSSignal:
   cdef gnss_signal_t _thisptr

src/signal.c
@@ -16,72 +16,36 @@
 
 #include <libswiftnav/signal.h>
 
-/*
-The implementation assumes that we have the following virtual list of codes:
+#define ASSERT_CODE_VALID(code) assert(((code) >= 0) && ((code) < CODE_COUNT))
 
-     prn         code
- 0: {1, CODE_GPS_L1CA},
- 1: {1, CODE_GPS_L2CM},
- 2: {2, CODE_GPS_L1CA},
- 3: {2, CODE_GPS_L2CM},
-...
-81: {137, CODE_SBAS_L1CA},
-82: {138, CODE_SBAS_L1CA},
-*/
+typedef struct {
+  enum constellation constellation;
+  u16 sat_count;
+  u16 sat_start;
+  const char *str;
+} code_table_element_t;
 
-static const u16 number_of_sats[] = {
-  NUM_SATS_GPS,
-  NUM_SATS_SBAS,
-};
-
-static const u16 first_sat_index[] = {
-  0,              /* GPS  */
-  NUM_SIGNALS_GPS,  /* SBAS */
-};
-
-static const u16 first_sat_id[] = {
-  GPS_FIRST_PRN,     /* GPS  */
-  SBAS_FIRST_PRN,    /* SBAS */
-};
-
-static const u8 codes_on_cons[] = {
-  NUM_CODES_GPS,   /* GPS  */
-  NUM_CODES_SBAS, /* SBAS */
-};
-
-static const u8 codes_on_gps[NUM_CODES_GPS] = {
-  CODE_GPS_L1CA,
-  CODE_GPS_L2CM,
-};
-
-static const u8 codes_on_sbas[NUM_CODES_SBAS] = {
-  CODE_SBAS_L1CA,
-};
-
-static const u8 local_code_indexes[CODE_COUNT] = {
-  [CODE_GPS_L1CA] = 0,
-  [CODE_GPS_L2CM] = 1,
-  [CODE_SBAS_L1CA] = 0,
-};
-
-static const char * code_strs[CODE_COUNT] = {
-  [CODE_GPS_L1CA] = "GPS L1CA",
-  [CODE_GPS_L2CM] = "GPS L2CM",
-  [CODE_SBAS_L1CA] = "SBAS L1CA"
+static const code_table_element_t code_table[CODE_COUNT] = {
+  [CODE_GPS_L1CA] =
+    {CONSTELLATION_GPS, NUM_SIGNALS_GPS_L1CA, GPS_FIRST_PRN, "GPS L1CA"},
+  [CODE_GPS_L2CM] =
+    {CONSTELLATION_GPS, NUM_SIGNALS_GPS_L2CM, GPS_FIRST_PRN, "GPS L2CM"},
+  [CODE_SBAS_L1CA] =
+    {CONSTELLATION_SBAS, NUM_SIGNALS_SBAS_L1CA, SBAS_FIRST_PRN, "SBAS L1CA"},
 };
 
 static const char * unknown_str = "?";
 
 gnss_signal_t construct_sid(enum code code, u16 sat)
 {
-  gnss_signal_t sid = { .code = code, .sat = sat};
+  gnss_signal_t sid = {.code = code, .sat = sat};
   return sid;
 }
 
 int sid_to_string(char *s, int n, gnss_signal_t sid)
 {
-  const char *code_str = (sid.code >= CODE_COUNT || sid.code == CODE_INVALID) ?
-      unknown_str : code_strs[sid.code];
+  const char *code_str = ((sid.code < 0) || (sid.code >= CODE_COUNT)) ?
+      unknown_str : code_table[sid.code].str;
 
   int nchars = snprintf(s, n, "%s %u", code_str, sid.sat);
   s[n-1] = 0;
@@ -90,113 +54,36 @@ int sid_to_string(char *s, int n, gnss_signal_t sid)
 
 bool sid_valid(gnss_signal_t sid)
 {
-  if (sid_to_constellation(sid) == CONSTELLATION_INVALID) {
+  if ((sid.code < 0) || (sid.code >= CODE_COUNT))
     return false;
-  }
 
-  if (sid.sat < first_sat_id[sid_to_constellation(sid)] ||
-      sid.sat >= first_sat_id[sid_to_constellation(sid)] +
-      number_of_sats[sid_to_constellation(sid)])
+  const code_table_element_t *e = &code_table[sid.code];
+  if ((sid.sat < e->sat_start) || (sid.sat >= e->sat_start + e->sat_count))
     return false;
 
   return true;
 }
 
-gnss_signal_t sid_from_index(u16 idx)
+gnss_signal_t sid_from_code_index(enum code code, u16 code_index)
 {
-  gnss_signal_t sid;
-  u8 code_idx = 0;
-  enum constellation cons = CONSTELLATION_INVALID;
-
-  if (idx < first_sat_index[CONSTELLATION_GPS] +
-      NUM_SATS_GPS * codes_on_cons[CONSTELLATION_GPS]) {
-    cons = CONSTELLATION_GPS;
-  } else if (idx < first_sat_index[CONSTELLATION_SBAS] +
-      NUM_SATS_SBAS * codes_on_cons[CONSTELLATION_SBAS]) {
-    cons = CONSTELLATION_SBAS;
-  } else {
-    assert("sid_from_index() invalid index");
-  }
-
-  code_idx = (idx - first_sat_index[cons]) % codes_on_cons[cons];
-
-  switch (cons) {
-  case CONSTELLATION_GPS:
-    sid.code = codes_on_gps[code_idx];
-    break;
-  case CONSTELLATION_SBAS:
-    if (sid.code != CODE_SBAS_L1CA)
-    sid.code = codes_on_sbas[code_idx];
-    break;
-  default:
-    break;
-  }
-
-  sid.sat = (idx - first_sat_index[cons]) /
-      codes_on_cons[cons] + first_sat_id[cons];
-
-  return sid;
+  assert((code >= 0) && (code < CODE_COUNT));
+  assert(code_index < code_table[code].sat_count);
+  return construct_sid(code, code_table[code].sat_start + code_index);
 }
 
-/******************************************************************************
-  sid    INDEX_GLOBAL    INDEX_CONSTELLATION I  NDEX_SAT_IN_CONS
-sat code
- 1  L1CA      0                   0                     0
- 1  L2CM      1                   1                     0
- 2  L1CA      2                   2                     1
- 2  L2CM      3                   3                     1
-...  ...    ...                 ...                   ...
- 31 L1CA     60                  60                    30
- 31 L2CM     61                  61                    30
- 32 L1CA     62                  62                    31
- 32 L2CM     63                  63                    31
-120 L1CA     64                   0                     0
-121 L1CA     65                   1                     1
-...  ...    ...                 ...                   ...
-137 L1CA     81                  17                    17
-138 L1CA     82                  18                    18
-
-'sat': Satellite ID in its constellation
-'code':  A combination of constellation, band and signal encoding
-'sid': Signal ID
-*******************************************************************************/
-
-u16 sid_to_index(gnss_signal_t sid, enum indexing_type it)
+u16 sid_to_code_index(gnss_signal_t sid)
 {
-  enum constellation cons = sid_to_constellation(sid);
-
-  switch (it) {
-  case INDEX_GLOBAL:
-    return first_sat_index[cons] +
-        (sid.sat - first_sat_id[cons]) * codes_on_cons[cons] +
-        local_code_indexes[sid.code];
-    break;
-  case INDEX_CONSTELLATION:
-    return sid_to_index(sid, INDEX_GLOBAL) - first_sat_index[cons];
-    break;
-  case INDEX_SAT_IN_CONS:
-    return sid.sat - first_sat_id[cons];
-    break;
-  default:
-    assert("unknown indexing type");
-    break;
-  }
-
-  return INVALID_SID_INDEX;
+  assert(sid_valid(sid));
+  return sid.sat - code_table[sid.code].sat_start;
 }
 
 enum constellation sid_to_constellation(gnss_signal_t sid)
 {
-  switch (sid.code) {
-  case CODE_GPS_L1CA:
-  case CODE_GPS_L2CM:
-    return CONSTELLATION_GPS;
-    break;
-  case CODE_SBAS_L1CA:
-    return CONSTELLATION_SBAS;
-    break;
-  default:
-    break;
-  }
-  return CONSTELLATION_INVALID;
+  return code_to_constellation(sid.code);
+}
+
+enum constellation code_to_constellation(enum code code)
+{
+  assert((code >= 0) && (code < CODE_COUNT));
+  return code_table[code].constellation;
 }

tests/check_signal.c
@@ -6,49 +6,90 @@
 
 #include <libswiftnav/signal.h>
 
-START_TEST(test_signal_from_index)
+#define ARRAY_COUNT(arr) ((sizeof(arr) / sizeof(arr[0])))
+
+static const struct code_data_element {
+  enum code code;
+  u16 sat_count;
+} code_data[] = {
+  {CODE_GPS_L1CA, NUM_SIGNALS_GPS_L1CA},
+  {CODE_GPS_L2CM, NUM_SIGNALS_GPS_L2CM},
+  {CODE_SBAS_L1CA, NUM_SIGNALS_SBAS_L1CA},
+};
+
+static const struct constellation_data_element {
+  enum constellation constellation;
+  u16 sat_count;
+  u8 code_count;
+} constellation_data[] = {
+  {CONSTELLATION_GPS, NUM_SIGNALS_GPS, NUM_CODES_GPS},
+  {CONSTELLATION_SBAS, NUM_SIGNALS_SBAS, NUM_CODES_SBAS},
+};
+
+START_TEST(test_signal_aggregates)
 {
-  for (u32 i=0; i<NUM_SIGNALS; i++) {
-    gnss_signal_t sid = sid_from_index(i);
-    fail_unless(sid_valid(sid), "signal from index %d not valid", i);
-    fail_unless(sid_to_index(sid, INDEX_GLOBAL) == i,
-                "signal from index %d to index failed", i);
+  fail_unless(ARRAY_COUNT(code_data) == CODE_COUNT,
+              "missing code entry in code_data[]");
+
+  fail_unless(ARRAY_COUNT(constellation_data) == CONSTELLATION_COUNT,
+              "missing constellation entry in constellation_data[]");
+
+  u8 constellation_code_counts[CONSTELLATION_COUNT];
+  memset(constellation_code_counts, 0, sizeof(constellation_code_counts));
+  for (u32 i=0; i<ARRAY_COUNT(code_data); i++) {
+    const struct code_data_element *e = &code_data[i];
+    enum constellation constellation = code_to_constellation(e->code);
+    constellation_code_counts[constellation]++;
   }
-  for (u32 i=0; i<NUM_SIGNALS_GPS; i++) {
-    gnss_signal_t sid = sid_from_index(i);
-    fail_unless(sid_valid(sid), "signal from index %d not valid", i);
-    fail_unless(sid_to_index(sid, INDEX_CONSTELLATION) == i,
-                "signal from index %d to index failed", i);
-  }
-  for (u32 i=0; i<NUM_SIGNALS_SBAS; i++) {
-    gnss_signal_t sid = sid_from_index(NUM_SIGNALS_GPS + i);
-    fail_unless(sid_valid(sid), "signal from index %d not valid", i);
-    fail_unless(sid_to_index(sid, INDEX_CONSTELLATION) == i,
-                "signal from index %d to index failed", i);
-  }
-  for (u32 i=0; i<NUM_SIGNALS_GPS; i++) {
-    gnss_signal_t sid = sid_from_index(i);
-    fail_unless(sid_valid(sid), "signal from index %d not valid", i);
-    fail_unless(sid_to_index(sid, INDEX_SAT_IN_CONS) == i / 2,
-                "signal from index %d to index failed", i);
-  }
-  for (u32 i=0; i<NUM_SIGNALS_SBAS; i++) {
-    gnss_signal_t sid = sid_from_index(NUM_SIGNALS_GPS + i);
-    fail_unless(sid_valid(sid), "signal from index %d not valid", i);
-    fail_unless(sid_to_index(sid, INDEX_SAT_IN_CONS) == i,
-                "signal from index %d to index failed: %d", i);
+  for (u32 i=0; i<ARRAY_COUNT(constellation_data); i++) {
+      const struct constellation_data_element *e = &constellation_data[i];
+      fail_unless(e->code_count == constellation_code_counts[e->constellation],
+                  "invalid code count definition for code %d",
+                  e->constellation);
   }
 }
 END_TEST
 
-START_TEST(test_signal_to_index)
+START_TEST(test_signal_from_index)
 {
-  const struct
+  for (u32 i=0; i<ARRAY_COUNT(code_data); i++) {
+    const struct code_data_element *e = &code_data[i];
+    enum code code = e->code;
+    u16 sat_count = e->sat_count;
+    for (u16 code_index = 0; code_index < sat_count; code_index++) {
+      gnss_signal_t sid = sid_from_code_index(code, code_index);
+      fail_unless(sid_valid(sid), "signal from code index not valid: "
+                                  "code %d code index %d",
+                                  code, code_index);
+      fail_unless(sid_to_code_index(sid) == code_index,
+                  "signal from code index to code index failed: "
+                  "code %d code index %d",
+                  code, code_index);
+    }
+  }
+}
+END_TEST
+
+START_TEST(test_signal_properties)
+{
+  const struct test_case
   {
     gnss_signal_t sid;
     bool valid;
     char str[SID_STR_LEN_MAX];
   } test_cases[] = {
+      {
+        .sid = {
+          .code = CODE_INVALID, .sat = 0
+        },
+        .valid = false
+      },
+      {
+        .sid = {
+          .code = CODE_COUNT, .sat = 0
+        },
+        .valid = false
+      },
       {
       .sid = {
           .code = CODE_INVALID, .sat = 1
@@ -134,23 +175,19 @@ START_TEST(test_signal_to_index)
       },
   };
 
-  fail_unless(sid_to_index(construct_sid(0, 0), -1) == (u16)INVALID_SID_INDEX,
-                    "sid_to_index INVALID_SID_INDEX test failed");
-
   for (u32 i=0; i<sizeof(test_cases) / sizeof(test_cases[0]); i++) {
-    bool valid = sid_valid(test_cases[i].sid);
-    fail_unless(test_cases[i].valid == valid,
-                "test signal %d validity incorrect", i);
+    const struct test_case *t = &test_cases[i];
+    bool valid = sid_valid(t->sid);
+    fail_unless(t->valid == valid, "test signal %d validity incorrect", i);
     if (valid) {
       gnss_signal_t sid =
-          sid_from_index(sid_to_index(test_cases[i].sid, INDEX_GLOBAL));
+          sid_from_code_index(t->sid.code, sid_to_code_index(t->sid));
+      fail_unless(sid_is_equal(t->sid, sid),
+                  "test signal %d code index conversion failed");
       char str[SID_STR_LEN_MAX] = {0};
-      u32 ret = sid_to_string(str, SID_STR_LEN_MAX, sid);
-      fail_unless(!strcmp(str,
-          test_cases[i].str) && ret == strlen(test_cases[i].str),
-          "signal to string \"%s\" failed", test_cases[i].str);
-      fail_unless(sid_is_equal(test_cases[i].sid, sid),
-                  "test signal %d index conversion failed");
+      u32 ret = sid_to_string(str, sizeof(str), sid);
+      fail_unless((strcmp(str, t->str) == 0) && (ret == strlen(t->str)),
+                  "signal to string \"%s\" failed", t->str);
     }
   }
 }
@@ -159,16 +196,23 @@ END_TEST
 START_TEST(test_signal_compare)
 {
   gnss_signal_t sids[NUM_SIGNALS];
-  for (u32 i=0; i<NUM_SIGNALS; i++) {
-    sids[i] = sid_from_index(i);
+  u32 signal_index = 0;
+
+  for (u32 i=0; i<ARRAY_COUNT(code_data); i++) {
+    const struct code_data_element *e = &code_data[i];
+    enum code code = e->code;
+    u16 sat_count = e->sat_count;
+    for (u16 code_index = 0; code_index < sat_count; code_index++) {
+      sids[signal_index++] = sid_from_code_index(code, code_index);
+    }
   }
 
   qsort(sids, NUM_SIGNALS, sizeof(gnss_signal_t), cmp_sid_sid);
 
-  for (u32 i=0; i<NUM_SIGNALS-1; i++) {
-    fail_unless(!sid_is_equal(sids[i+1], sids[i]),
+  for (u32 i=1; i<NUM_SIGNALS; i++) {
+    fail_unless(!sid_is_equal(sids[i], sids[i-1]),
                 "signal index %d not unique", i);
-    fail_unless(sid_compare(sids[i+1], sids[i]) > 0,
+    fail_unless(sid_compare(sids[i], sids[i-1]) > 0,
                 "signal index %d not in order", i);
   }
 }
@@ -176,14 +220,22 @@ END_TEST
 
 START_TEST(test_signal_construction)
 {
-  for (u32 i=0; i<NUM_SIGNALS; i++) {
-    gnss_signal_t sid = sid_from_index(i);
-    fail_unless(sid_valid(sid), "signal from index %d not valid", i);
-    gnss_signal_t csid = construct_sid(sid.code, sid.sat);
-    fail_unless(sid_valid(csid), "constructed signal from index %d not valid",
-        i);
-    fail_unless(!sid_compare(sid, csid),
-                "signal from index %d to index failed", i);
+  for (u32 i=0; i<ARRAY_COUNT(code_data); i++) {
+    const struct code_data_element *e = &code_data[i];
+    enum code code = e->code;
+    u16 sat_count = e->sat_count;
+    for (u16 code_index = 0; code_index < sat_count; code_index++) {
+      gnss_signal_t sid = sid_from_code_index(code, code_index);
+      gnss_signal_t csid = construct_sid(sid.code, sid.sat);
+      fail_unless(sid_valid(csid),
+                  "constructed signal not valid: "
+                  "code %d code index %d",
+                  code, code_index);
+      fail_unless(sid_is_equal(sid, csid),
+                  "constructed signal mismatch: "
+                  "code %d code index %d",
+                   code, code_index);
+    }
   }
 }
 END_TEST
@@ -193,8 +245,9 @@ Suite* signal_test_suite(void)
   Suite *s = suite_create("Signal");
 
   TCase *tc_core = tcase_create("Core");
+  tcase_add_test(tc_core, test_signal_aggregates);
   tcase_add_test(tc_core, test_signal_from_index);
-  tcase_add_test(tc_core, test_signal_to_index);
+  tcase_add_test(tc_core, test_signal_properties);
   tcase_add_test(tc_core, test_signal_compare);
   tcase_add_test(tc_core, test_signal_construction);
   suite_add_tcase(s, tc_core);

akleeman/libswiftnav

commit f148a87ae272eb74bc2843ba3c51a053f5f65ccf

Fixed Bindings after error introduced in 2c3bdf602b7fc.
signal.pyx::GNSSSignal included some methods that didn't resolve
when loading the bindings in python.  Since these methods weren't
being used, they were simply removed.

python/swiftnav/signal.pyx
@@ -39,25 +39,12 @@ cdef class GNSSSignal:
   def is_valid(self):
     return sid_valid(self._thisptr)
 
-  def to_code_index(self):
-    return sid_to_code_index(self._thisptr)
-
-  def from_code_index(self, code, code_index):
-    self._thisptr = sid_from_code_index(code, code_index)
-
-  def to_constellation(self):
-    return sid_to_constellation(self._thisptr)
-
   def to_string(self):
     cdef u8 n = 255
     cdef char s[255]
     cdef s8 length = sid_to_string(&s[0], n, self._thisptr)
     return s[:length].decode('UTF-8')
-
-def signal_from_code_index(code, code_index):
-  sid = GNSSSignal()
-  sid._thisptr = sid_from_code_index(code, code_index)
-  return sid
+  
 
 cdef mk_signal_array(py_signals, u8 n_c_signals, gnss_signal_t *c_signals):
   """Given an array of python SingleDiffs, copies their contents to an
@@ -73,6 +60,7 @@ cdef mk_signal_array(py_signals, u8 n_c_signals, gnss_signal_t *c_signals):
     sd_ = (<GNSSSignal> signal)._thisptr
     memcpy(&c_signals[i], &sd_, sizeof(gnss_signal_t))
 
+
 cdef read_signal_array(u8 n_c_signals, gnss_signal_t *c_signals):
   """Given an array of c gnss_signal_t's, returns an array of GNSSSignals.
 
@@ -83,4 +71,3 @@ cdef read_signal_array(u8 n_c_signals, gnss_signal_t *c_signals):
     sd_ = (<GNSSSignal> signal)._thisptr
     memcpy(&sd_, &c_signals[i], sizeof(gnss_signal_t))
   return py_signals
-